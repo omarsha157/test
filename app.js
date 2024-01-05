2cae1b573ecd6d67bdbbe4a1aad6a677e2fdc93b
omarsha157 <omarsha157@gmail.com>
2024-01-05 18:20:27 +0400

    first

---
+++ app.js
@@ -0,0 +1,12 @@
+const copyrightYear = document.querySelector('.curr-year')
+
+const date = new Date()
+const currYear = date.getFullYear()
+
+copyrightYear.innerText = currYear
+
+const tl = gsap.timeline()
+
+tl.from('.nav', {y: '-100%'})
+tl.from('.main-content-wrapper', {x:'-100%'})
+tl.from('.footer',{y:'100%'})
