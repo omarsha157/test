22b30a39a814f9de981fb372bb403dcbae5dde7f
omarsha157 <omarsha157@gmail.com>
2024-01-05 19:15:42 +0400

    second

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const currYear = date.getFullYear()
 
 copyrightYear.innerText = currYear
 
-const tl = gsap.timeline()
+const tl = gsap.timeline({duration: 0.3})
 
 tl.from('.nav', {y: '-100%'})
 tl.from('.main-content-wrapper', {x:'-100%'})
