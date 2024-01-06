90819268df5dc35047d91e214338ab30877f642d
omarsha157 <omarsha157@gmail.com>
2024-01-06 19:33:05 +0400

    third

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+const video = document.querySelector('#myVideo')
 const copyrightYear = document.querySelector('.curr-year')
 
 const date = new Date()
 const currYear = date.getFullYear()
 
 copyrightYear.innerText = currYear
+
+video.addEventListener('ended', () => {
+    video.classList.add('low-opacity')
+})
+
+video.addEventListener('play', () => {
+    video.classList.remove('low-opacity')
+})
 
 const tl = gsap.timeline({duration: 0.3})
 
